545a6522b3007a0c7afa7d10da821edd0bd1c62d
carlo <carlo.vanderstoep@gmail.com>
2020-01-28 19:36:38 +0100

    included depth in minimax algorithm to ensure shortest route to win.

--- find_best_move.py
+++ find_best_move.py
@@ -1,12 +1,17 @@
+number_of_minimax_calls = 0
+
 def find_best_move(board):
     # finds the best move for O (opponent; computer)
+    #include alpha-beta-pruning
     best_value = +1000
     best_move = -1
+    alpha = -1000
+    beta = +1000
     for move in board.legal_moves():
         board.board[move] = board.current_player
         depth = 0
         maximizing_player = True
-        current_score = mini_max(board, depth, maximizing_player)  # O has made the move, now evaluate X
+        current_score = mini_max(board, depth, maximizing_player, alpha, beta)  # O has made the move, now evaluate X
         if current_score is not None and current_score < best_value:
             best_move = move
             best_value = current_score
@@ -14,7 +19,10 @@ def find_best_move(board):
     return best_move
 
 
-def mini_max(board, depth, isMaximizingPlayer):
+def mini_max(board, depth, isMaximizingPlayer, alpha, beta):
+    global number_of_minimax_calls
+    number_of_minimax_calls +=1
+    print("nc= ", number_of_minimax_calls)
     current_score = board.return_score()
     if current_score is not None:  # this means the game is ended (win/lose/draw)
         if current_score == 10:
@@ -27,18 +35,20 @@ def mini_max(board, depth, isMaximizingPlayer):
         current_player = 'X'
         for move in board.legal_moves():
             board.board[move] = current_player
-            best_value = max(best_value, mini_max(board, depth + 1, not isMaximizingPlayer))
+            best_value = max(best_value, mini_max(board, depth + 1, not isMaximizingPlayer, alpha, beta))
+            alpha = max(alpha, best_value)
             board.board[move] = move  # undo the move
+            if beta <= alpha:
+                break
         return best_value
     else:
         best_value = 1000
         current_player = 'O'
         for move in board.legal_moves():
             board.board[move] = current_player
-            best_value = min(best_value, mini_max(board, depth + 1, not isMaximizingPlayer))
+            best_value = min(best_value, mini_max(board, depth + 1, not isMaximizingPlayer, alpha, beta))
+            beta = min(beta, best_value)
             board.board[move] = move  # undo the move
+            if beta <= alpha:
+                break
         return best_value
-
-
-
-
